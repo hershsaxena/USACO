ea86a80d1b558504978ce55af8dc3e05df4f4790
hershsaxena <hershvsaxena@gmail.com>
2024-09-01 16:23:51 -0700

    Finished Convention II

--- src/conventionTwo.java
+++ src/conventionTwo.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class conventionTwo {
     public static void main(String[] args) throws IOException {
@@ -8,7 +8,38 @@ public class conventionTwo {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int numCows = Integer.parseInt(st.nextToken());
+        Cow[] cowsSortedTime = new Cow[numCows];
+        int cowIndex = 1;
+        Queue<Cow> cows = new PriorityQueue<>(numCows, Comparator.comparingInt(a -> a.seniority));
+        for (int i = 0; i < numCows; i++) {
+            st = new StringTokenizer(br.readLine());
+            cowsSortedTime[i] = new Cow(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(cowsSortedTime, Comparator.comparingInt(a -> a.arrival));
+        cows.add(cowsSortedTime[0]);
+        int time = cowsSortedTime[0].arrival;
+        int max = 0;
 
+        while (cowIndex < numCows || !cows.isEmpty()) {
+            Cow cur = cows.remove();
+            time = Math.max(time, cur.arrival);
+            max = Math.max(max, time - cur.arrival);
+            time += cur.eatingTime;
+            if (cows.isEmpty() && cowIndex < numCows) {
+                int curTime = cowsSortedTime[cowIndex].arrival;
+
+                do {
+                    cows.add(cowsSortedTime[cowIndex]);
+                    cowIndex++;
+                } while (cowIndex < numCows && cowsSortedTime[cowIndex].arrival == curTime);
+            }
+            while (cowIndex < numCows && cowsSortedTime[cowIndex].arrival <= time) {
+                cows.add(cowsSortedTime[cowIndex]);
+                cowIndex++;
+            }
+        }
+
+        pw.println(max);
         pw.close();
     }
 
